81024db8c1017f3dfcd57fc5108c9a9766e61e02
Thang Le Quoc <silverhair.guy@gmail.com>
2017-11-28 21:05:37 +0700

    init commit

---
+++ src/main/java/com/thanglequoc/controllers/HomeController.java
@@ -0,0 +1,5 @@
+package com.thanglequoc.controllers;
+
+public class HomeController {
+
+}
---
+++ src/main/java/com/thanglequoc/entities/EmployeeEntity.java
@@ -0,0 +1,15 @@
+package com.thanglequoc.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class EmployeeEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+}
